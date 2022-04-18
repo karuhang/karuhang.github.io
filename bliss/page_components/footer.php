e04307e1cc86a1573e51732e1bd77826f678e4d4
Karuhanga@ Innocent <ikaruhanga182@gmail.com>
2022-04-18 14:06:59 +0300

    Revert "ahh"
    This reverts commit c50afdf86d210848b24da836f11a4a43a6e75d52.

---
+++ bliss/page_components/footer.php
@@ -0,0 +1,77 @@
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h3>Bliss Rich Unisex Saloon</h3>
+            <p>
+
+              High Street <br>
+              BNK SHOPPING MALL-FIRST FLOOR-Room NO; K OO1, 002, 003<br>
+              Mbarara City<br><br>
+              <strong><i style="color: green;"class="bu bi-whatsapp"></i>&nbsp;</strong> +256758145285<br>
+              <strong><i style="color: orange;"class="bu bi-phone"></i>&nbsp;</strong> +256784119541 | +256702576034 | +256701487820<br>
+              <strong><i style="color: red;"class="bu bi-envelope"></i>&nbsp;</strong> info@theblissrich.com<br>
+            </p>
+          </div>
+
+          <div class="col-lg-2 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Our Services</h4>
+            <ul>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Hair Cuts</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Hair Styling</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Waxing</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Pedicure and Manicure</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Body massage</a></li>
+              <li><i class="bx bx-chevron-right"></i> <a href="#">Facial treatment</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-4 col-md-6 footer-newsletter">
+            <h4>Join Our Newsletter</h4>
+            <p>Subscribe to receive updates on special offers and new hair information</p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container d-md-flex py-4">
+
+      <div class="me-md-auto text-center text-md-start">
+        <div class="copyright">
+          &copy; Copyright 2022 <strong><span>Blissrich.com</span></strong>. All Rights Reserved
+        </div>
+        <div class="credits">
+          <!-- All the links in the footer should remain intact. -->
+          <!-- You can delete the links only if you purchased the pro version. -->
+          <!-- Licensing information: https://bootstrapmade.com/license/ -->
+          <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/company-free-html-bootstrap-template/ -->
+          Designed by <a href="#">Underscore Technologies Mbarara</a>
+        </div>
+      </div>
+      <div class="social-links text-center text-md-right pt-3 pt-md-0">
+        <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+        <!-- <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a> -->
+        <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+      </div>
+    </div>
+  </footer>
---
+++ bliss/page_components/header.php
@@ -0,0 +1,48 @@
+<header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index"><span>Bliss</span>Rich</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto me-lg-0"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a href="index" class="active">Home</a></li>
+
+          <li class="dropdown"><a href="#"><span>About</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <!-- <li><a href="about">About Us</a></li> -->
+              <li><a href="about-us">About Bliss Rich.</a></li>
+              <li><a href="#">Team</a></li>
+              <!-- <li><a href="testimonials.html">Testimonials</a></li> -->
+<!--               <li class="dropdown"><a href="#"><span>Deep Drop Down</span> <i class="bi bi-chevron-right"></i></a>
+                <ul>
+                  <li><a href="#">Deep Drop Down 1</a></li>
+                  <li><a href="#">Deep Drop Down 2</a></li>
+                  <li><a href="#">Deep Drop Down 3</a></li>
+                  <li><a href="#">Deep Drop Down 4</a></li>
+                  <li><a href="#">Deep Drop Down 5</a></li>
+                </ul>
+              </li> -->
+            </ul>
+          </li>
+
+          <li><a href="services">Services</a></li>
+          <li><a href="#">Fashions</a></li>
+<!--           <li><a href="pricing.html">Pricing</a></li>
+ --><!--           <li><a href="blog.html">Blog</a></li>
+ -->          <li><a href="contact">Contact</a></li>
+
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <div class="header-social-links d-flex">
+        <a href="#" class="twitter"><i class="bu bi-twitter"></i></a>
+        <a href="#" class="facebook"><i class="bu bi-facebook"></i></a>
+        <a href="#" class="instagram"><i class="bu bi-instagram"></i></a>
+        <a href="#" class="linkedin"><i class="bu bi-linkedin"></i></i></a>
+      </div>
+
+    </div>
+  </header>
